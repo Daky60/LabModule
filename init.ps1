c869995c627ddf6b9b643fdcdc0003f83978460e
Daky <mail@sebfrodin.se>
2021-02-25 18:37:28 +0100

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,16 +1,50 @@
+$config = ".\conf.psd1"
+$module = ".\LabModule.psm1"
+
+
 if (Get-Module | Where-Object{$_.Name -eq "LabModule"}) {
     Remove-Module LabModule
 }
-Import-Module .\LabModule.psm1
+Import-Module $module
 
-#Get-Content .\config.psd1
-$config = Import-PowerShellDataFile .\config.psd1
-Write-Host($config.AllNodes)
 
-#Build-LabSwitch -Name "$labSuffix-Switch" -Type $SwitchType
-#Build-LabVM @VM_DC -Username $User -Password $Pass
-#Build-Forest @Forest -Username $User -Password $Pass
-#Build-LabVM @VM_SRV1 -Username $User -Password $Pass
+try {
+    if (!(Test-Path $module)) {
+        throw "Module file not found. Edit init.ps1"
+    }
+    if (Test-Path $config) {
+        $config = Import-PowerShellDataFile $config
+    }
+    else {
+        throw "Config file not found. Edit init.ps1"
+    }
+    foreach ($i in $config.ActionList) {
+        if ($i.ActionType) {
+            switch ($i.ActionType) {
+                "Switch" {
+                    $i.Remove("ActionType")
+                    Build-LabSwitch @i
+                }
+                "VM" {
+                    $i.Remove("ActionType")
+                    Build-LabVM @i
+                }
+                "Forest" {
+                    $i.Remove("ActionType")
+                    Build-LabForest @i
+                }
+                default { "ActionType must be Switch, VM or Forest" }
+            }
+        }
+        else {
+            throw "Missing ActionType. Edit config file"
+        }
+    }
+}
+catch {
+    throw $_.Exception.Message
+}
+
 
 if (Get-Module | Where-Object{$_.Name -eq "LabModule"}) {
     Remove-Module LabModule
